57eb5ddbe8db9af80d95361f11a5edc238a55ea6
Sanuja Cooray <sanujac@live.com>
2022-10-25 20:21:19 +0530

    Product Feed Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
-import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       {/* Header */}
-      <Header />
-      {/* Banner */}
-      {/* Product Feed */}
-    </div>
+      < Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Product Feed */}
+        <ProductFeed products={products} />
+      </main>
+    </div >
   );
 }
+
+
+// Switches Server Side Rendering On 
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(res => res.json())
+  return { props: { products } }
+}
+
+/// https://fakestoreapi.com/products
