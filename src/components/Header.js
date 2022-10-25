ad0c0b9a1a3fd08bc3e43e04fbbfb02fd2b8563e
Sanuja Cooray <sanujac@live.com>
2022-10-25 18:23:40 +0530

    Header Actually Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,17 +32,27 @@ export default function Header() {
                         <p>Returns</p>
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
-                    
+
                     <div className="relative link flex items-center">
-                    <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">4</span>
-                        <ShoppingCartIcon className="h-10"/>
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">4</span>
+                        <ShoppingCartIcon className="h-10" />
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
                     </div>
                 </div>
             </div>
 
             {/* Bottom Nav */}
-            <div>
+            <div className="flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6">
+                <p className="link flex items-center"><Bars4Icon className="h-6 mr-1" /> All</p>
+                <p className="link">Prime Video</p>
+                <p className="link">Amazon Business</p>
+                <p className="link">Today's Deals</p>
+                <p className="link hidden lg:inline-flex">Electronics</p>
+                <p className="link hidden lg:inline-flex">Food & Grocery</p>
+                <p className="link hidden lg:inline-flex">Prime</p>
+                <p className="link hidden lg:inline-flex">Buy Again</p>
+                <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+                <p className="link hidden lg:inline-flex">Health & Personal Care</p>
             </div>
         </header>)
 }
