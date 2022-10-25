34abcf982daeb035136ff492573f46c61e36caeb
Sanuja Cooray <sanujac@live.com>
2022-10-25 18:17:34 +0530

    Header done

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { Bars4Icon, MagnifyingGlassIcon, ShoppingCartIcon } from "@heroicons/react/24/outline"
+export default function Header() {
+    return (
+        <header>
+            {/* Top Nav */}
+            <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+                {/* Logo */}
+                <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+                    <Image
+                        src="https://links.papareact.com/f90"
+                        width={150}
+                        height={40}
+                        objectFit="contain"
+                        className="cursor-pointer"
+                    />
+                </div>
+
+                {/* Search Bar */}
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow bg-yellow-400 hover:bg-yellow-500 cursor-pointer">
+                    <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none" type="text" />
+                    <MagnifyingGlassIcon className="h-12 p-4" />
+                </div>
+
+                {/* Right */}
+                <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                    <div className="link">
+                        <p>Hello Sanuja Cooray</p>
+                        <p className="font-extrabold md:text-sm">Account & Lists</p>
+                    </div>
+                    <div className="link">
+                        <p>Returns</p>
+                        <p className="font-extrabold md:text-sm">& Orders</p>
+                    </div>
+                    
+                    <div className="relative link flex items-center">
+                    <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">4</span>
+                        <ShoppingCartIcon className="h-10"/>
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Bottom Nav */}
+            <div>
+            </div>
+        </header>)
+}
