991eef405eeaeb40ad7eea7daca17dee0cc2268a
Sanuja Cooray <sanujac@live.com>
2022-10-25 16:12:55 +0530

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,85 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Amazon 2.0</title>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+      <center className="grid place-items-center mt-10">
+        <h1 className="text-5xl">Lets build Amazon 2.0</h1>
+        <h2>This is your starter template!</h2>
+        <br />
+        <h3 className="font-bold">
+          We will be using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
+        </h3>
+        <i>(Dont worry, its all setup and ready to use!)</i>
+        <h4>Get Ready, Get Set, GO!!!</h4>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+        <h5 className="mb-10">#PAPAFAM</h5>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+        <div className="bg-red-300 p-10">
+          <p className="font-bold">
+            Dont forget to register for the challenge here!
+          </p>
+          <p>👇👇👇</p>
           <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="https://www.papareact.com/secret-challenge"
+            className="text-blue-400 underline p-3 font-bold"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            CLICK HERE TO REGISTER NOW
           </a>
         </div>
-      </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+        <p className="mt-24">Built with 💙 by Sonny Sangha (PAPA REACT)</p>
+      </center>
+      {/* ---- ---- */}
     </div>
-  )
+  );
 }
-
-export default Home
